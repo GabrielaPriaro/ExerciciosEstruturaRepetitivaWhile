b2ad8a00188acc6c124d8beb92216e9a59a47f58
Gabriela <gabriela.priaro@gmail.com>
2024-11-21 11:55:25 +0000

    Add project files.

---
+++ ExerciciosEstruturaRepetitivaWhile/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciciosEstruturaRepetitivaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
